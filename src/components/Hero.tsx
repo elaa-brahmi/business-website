3c27ff41410bf2c5a156fdb5746299b0147e1da1
elaa <elaa.brahmii@gmail.com>
2025-11-12 21:48:52 +0100

    change here section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,8 +15,9 @@ export const Hero = () => {
           for{" "}
           <h2 className="inline">
             <span className="inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text">
-              everyone
+              your
             </span>{" "}
+            startup
             
           </h2>
         </main>
